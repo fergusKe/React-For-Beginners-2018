2fe0fefe057444f2c6f85a35351669e79454a4e6
fergusKe <fergus.jingtai@gmail.com>
2018-03-01 22:58:22 +0800

    3. Creating our First Components

---
+++ src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class StorePicker extends Component {
+	render() {
+		return (
+			<div>
+				StorePicker
+			</div>
+		)
+	}
+}
+
+export default StorePicker
