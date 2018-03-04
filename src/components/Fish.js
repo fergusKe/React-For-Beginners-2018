fcffcaa7c138b0262e4abffaa79a8fc84029df46
fergusKe <fergus.jingtai@gmail.com>
2018-03-05 03:48:13 +0800

    23. Component Validation with PropTypes

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { formatPrice } from "../helpers"
 
 class Fish extends Component {
-	handleClick = () => {
-		this.props.addToOrder(this.props.index)
+	static propTypes = {
+		details: PropTypes.shape({
+			image: PropTypes.string,
+			name: PropTypes.string,
+			desc: PropTypes.string,
+			status: PropTypes.string,
+			price: PropTypes.number
+		}),
+		addToOrder: PropTypes.func
 	}
-
 	render() {
 		const { image, name, price, desc, status } = this.props.details
 		const isAvailable = status === 'available'
@@ -20,7 +27,7 @@ class Fish extends Component {
 				<p>{desc}</p>
 				<button
 					disabled={!isAvailable}
-					onClick={this.handleClick}
+					onClick={() => this.props.addToOrder(this.props.index)}
 				>
 					{isAvailable ? "Add To Order" : "Sold Out!"}
 				</button>
