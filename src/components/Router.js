ba4f9faca8d36f2fca771c9b83f7e04883600b56
Fergus <fergus.jingtai@gmail.com>
2018-03-07 14:19:40 +0800

    Use HashRouter

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,18 +1,18 @@
 
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 import App from './App'
 import StorePicker from './StorePicker'
 import NotFound from './NotFound'
 
 const Router = () => (
-	<BrowserRouter>
+	<HashRouter>
 		<Switch>
 			<Route exact path="/" component={StorePicker} />
 			<Route path="/store/:storeId" component={App} />
 			<Route component={NotFound} />
 		</Switch>
-	</BrowserRouter>
+	</HashRouter>
 )
 
 export default Router
